3b2ff1be19ad17a7a08e6a056478a590a7a76a82
wangyz <100399172+WangYZ1608@users.noreply.github.com>
2023-05-26 14:24:30 +0800

    ND for CIFAR-100

---
+++ CIFAR/emb_visual.sh
@@ -0,0 +1,6 @@
+python3 emb_fea_distribution.py \
+        --model_name resnet56_cifar \
+        --model_weights 'MODEL WEIGHT PATH' \
+        --emb_size 64 \
+        --dataset 'cifar100' \
+        --batch_size 128
